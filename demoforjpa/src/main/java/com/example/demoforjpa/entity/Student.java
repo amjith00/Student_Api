be9a8107e579829192eaab058ad06972b46105f7
amjith00 <amjith9900@gmail.com>
2025-11-24 18:07:40 +0530

    initial commit : student api

---
+++ demoforjpa/src/main/java/com/example/demoforjpa/entity/Student.java
@@ -0,0 +1,52 @@
+package com.example.demoforjpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String studentName;
+	private String studentEmail;
+	private String studentAddress;
+	public long getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	public Student() {
+		super();
+	
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", studentName=" + studentName + ", studentEmail=" + studentEmail
+				+ ", studentAddress=" + studentAddress + "]";
+	}
+
+	
+}
